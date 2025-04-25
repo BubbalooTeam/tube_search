4ac1310663d5f40be541cc2471518dba434412df
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2025-04-25 18:42:42 +0000

    feat(enums): Added all enums for the best experience.

--- tube_search/utils/enums.py
+++ tube_search/utils/enums.py
@@ -1,7 +1,27 @@
-from enum import StrEnum
+from enum import Enum, StrEnum
 
 class TubeEnums(StrEnum):
-    VIDEO_ELEMENT = "videoRenderer"
+    HASHTAG_BROWSER = "FEhashtag"
     CHANNEL_ELEMENT = "channelRenderer"
+    HASHTAG_ELEMENT = "hashtagTileRenderer"
     PLAYLIST_ELEMENT = "playlistRenderer"
+    PLAYLIST_VIDEO_ELEMENT = "playlistVideoRenderer"
+    SEARCH_ELEMENT = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
     SHELF_ELEMENT = "shelfRenderer"
+    VIDEO_ELEMENT = "videoRenderer"
+    CONTINUATION_ITEM = "continuationItemRenderer"
+    RICH_ITEM = "richItemRenderer"
+    ITEM_SECTION = "itemSectionRenderer"
+    PLAYER_RESPONSE = "playerResponse"
+    PLAYLIST_PRIMARY_SIDEBAR = "playlistSidebarPrimaryInfoRenderer"
+    PLAYLIST_SECONDARY_SIDEBAR = "playlistSidebarSecondaryInfoRenderer"
+
+class TubePathsEnums(Enum):
+    HASHTAG_VIDEOS = ['contents', 'twoColumnBrowseResultsRenderer', 'tabs', 0, 'tabRenderer', 'content', 'richGridRenderer', 'contents']
+    HASHTAG_CONTINUATION = ['onResponseReceivedActions', 0, 'appendContinuationItemsAction', 'continuationItems']
+    CONTENTS = ['contents', 'twoColumnSearchResultsRenderer', 'primaryContents', 'sectionListRenderer', 'contents']
+    FALLBACK_CONTENT = ['contents', 'twoColumnSearchResultsRenderer', 'primaryContents', 'richGridRenderer', 'contents']
+    CONTINUATION_CONTENT = ['onResponseReceivedCommands', 0, 'appendContinuationItemsAction', 'continuationItems']
+    CONTINUATION_TOKEN = ['continuationItemRenderer', 'continuationEndpoint', 'continuationCommand', 'token']
+    PLAYLIST_INFO = ['response', 'sidebar', 'playlistSidebarRenderer', 'items']
+    PLAYLIST_VIDEOS = ['response', 'contents', 'twoColumnBrowseResultsRenderer', 'tabs', 0, 'tabRenderer', 'content', 'sectionListRenderer', 'contents', 0, 'itemSectionRenderer', 'contents', 0, 'playlistVideoListRenderer', 'contents']
