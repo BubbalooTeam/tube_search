8743fb499f492a72e4001b06a2c17f4460425146
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2025-04-25 18:18:38 +0000

    + +

---
+++ tube_search/utils/enums.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+class TubeEnums(StrEnum):
+    VIDEO_ELEMENT = "videoRenderer"
+    CHANNEL_ELEMENT = "channelRenderer"
+    PLAYLIST_ELEMENT = "playlistRenderer"
+    SHELF_ELEMENT = "shelfRenderer"
