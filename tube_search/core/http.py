e84cd12f7fed22e5163a77195cce26cdae6dcc5c
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2025-04-25 18:17:37 +0000

    feat(http): Add http core.

---
+++ tube_search/core/http.py
@@ -0,0 +1,24 @@
+from httpx import AsyncClient, Timeout
+
+class HTTPCore:
+    def __init__(self):
+        self.http = AsyncClient(
+            http2=True,
+            timeout=Timeout(
+                timeout=20,
+                pool=None,
+            ),
+        )
+        self.payload = dict(
+            context=dict(
+                client=dict(
+                    clientName="WEB",
+                    clientVersion="2.20210224.06.00",
+                    newVisitorCookie=True,
+                ),
+                user=dict(
+                    lockedSafetyMode=False,
+                )
+            )
+        )
+        self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0"
