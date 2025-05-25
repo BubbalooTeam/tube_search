b9831d2d4b208cd7d08895d67e9f967a59a13bbb
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2025-05-25 19:19:21 +0000

    + +

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="tube-search",
-    version="0.0.2.1",
-    license="MIT License",
-    author="BubbalooTeam",
-    author_email="bubbalooteam@proton.me",
-    keywords=["tube search", "tube_search"],
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    description="🔎 Search for videos, channels and playlists on YouTube quickly and easily.",
-    packages=["tube_search"],
-    install_requires=[
-        "httpx[http2]",
-    ],
+    packages=find_packages("tube_search")
 )
